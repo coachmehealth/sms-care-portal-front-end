dbb49b918c8f2d1ee8d1f37ff1c2c29556925880
cesar-vega-unosquare <cesarv@coachmehealth.org>
2021-05-26 10:51:44 -0500

    Disable eslint no-console on build.js and start.js

--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint no-console: 0*/
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
