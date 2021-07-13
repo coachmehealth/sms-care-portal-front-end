13f74138e63a7b386a166791418fe852c56839d5
g-vega-cl <13635035+g-vega-cl@users.noreply.github.com>
2021-07-13 12:46:07 -0500

    Fix coach dropdown (#31)
    * Fix coach dropdown by replacing fancy component with standard select with options.

---
+++ src/interfaces/coach.ts
@@ -0,0 +1,11 @@
+declare interface ICoach {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  email: string;
+  password: string;
+  refreshToken: string;
+}
+
+export default ICoach;
